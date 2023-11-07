e8cfcbbb45e18da3f07700b462ccd1413a465219
BhargaviATP <yadati.bhargavi@gmail.com>
2023-11-07 13:54:49 +0530

    This is about Fork

---
+++ AdvancedAutomation/src/test/java/bookToCart/forksampleclass.java
@@ -0,0 +1,14 @@
+package bookToCart;
+
+import org.testng.annotations.Test;
+
+import genericUtilities.BaseClass;
+@Test
+public class forksampleclass extends BaseClass{
+		public void fork() {
+			driver.navigate().to("https://www.facebook.com");
+			
+		}
+		
+	}
+
